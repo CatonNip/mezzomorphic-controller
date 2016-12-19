05359fe1533f82302adb1136b58b4281ef6875d7
Alex Nelson <caminelson.cn@gmail.com>
2016-12-19 11:12:19 -0800

    abridge json response to only send the id and auth_code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
+  # protect_from_forgery only:  [:update, :destroy]
 
   # GET /users
   def index
@@ -18,7 +19,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created, location: @user
+      render json: {id: @user.id, auth_key: 0}, status: :created, location: @user
     else
       render json: @user.errors, status: :unprocessable_entity
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :channels
   serialize :channels, Array
+
+  validates :email, presence: true
 end
